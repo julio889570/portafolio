d83155e7617f3825c62bb102ae76550a4dc4cf13
julio889570 <julio889570@gmail.com>
2024-03-19 23:26:06 -0400

    first

---
+++ src/componentes/NavBar.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react'
+import {Navbar, Container} from 'react-bootstrap'
+
+
+function NavBar() {
+     const [activelink, setActiveLink] = useState('home');
+     const [scrolled, setScrolled] = useState(false);
+     
+     useEffect(()=>{
+        const onScroll = () =>{
+            if (windows.scrolly > 50){
+                setScrolled(true);
+            }else{
+                setScrolled(false);
+            }
+        }
+        windows.addEventListener('scroll', onScroll);
+
+        return () => windows.removeEventListener('scroll', onScroll);
+     }, [])
+
+  return (
+    <Navbar className="bg-body-tertiary">
+    <Container>
+      <Navbar.Brand href="#home">
+        <img src={''} alt="Logo" />
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav">
+        <span className='navbar-toggler-icon'></span>
+        </Navbar.Toggle>
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="me-auto">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#link">Skill</Nav.Link>
+          <Nav.Link href="#projects">Projects</Nav.Link>
+
+        </Nav>
+        <span className="nav-text"> l0
+        <div className="social-icon">
+            <a href="#"><img src={''} alt="" /></a>
+            <a href="#"><img src={''} alt="" /></a>
+            <a href="#"><img src={''} alt="" /></a>
+        </div>
+        <button className='vvd' onClick={()=> console.log('connect')}><span>let's connect</span></button>
+        </span>
+
+      </Navbar.Collapse>
+    </Container>
+  </Navbar>
+  )
+}
+
+export default NavBar
